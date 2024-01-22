49acb22d7c6086bb198cf9825279148ec0326c7a
Magifire <65313702+Magifire@users.noreply.github.com>
2024-01-22 16:48:49 -0500

    eseeze fdaxcv

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	if direction and (get_multiplayer_authority() == multiplayer.multiplayer_peer.get_unique_id()):
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
